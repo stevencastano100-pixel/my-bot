b8873dc4d6fc95dd294d119d9488cad30d939792
unknown <stevencastano100@gmail.com>
2026-04-02 17:56:17 -0400

    add web server for render

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Web server ready"));
 require('dotenv').config();
 const {
   Client,
